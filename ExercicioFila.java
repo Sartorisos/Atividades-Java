8180d01779e33c6e8f4d7916ec3e25a7b9111ec4
Sartorisos <108590345+Sartorisos@users.noreply.github.com>
2023-04-24 17:24:28 -0300

    Add files via upload

---
+++ ExercicioFila.java
@@ -0,0 +1,92 @@
+package Atividades3;
+import java.util.*;
+import java.util.Scanner;
+
+/*
+Escreva um programa Java contendo uma Collection Queue (Fila) de Objetos da Classe String, 
+para organizar a ordem de chegada dos Clientes de um Banco. O programa deverá ter um 
+Menu que aceitará as opções 0, 1, 2 e 3:
+1: Adicionar um novo Cliente na fila. Deve solicitar o nome do Cliente.
+2: Listar todos os Clientes na fila
+3: Chamar (retirar) uma pessoa da fila 
+0: O programa deve ser finalizado. 
+Caso a fila esteja vazia, o programa deverá informar que a fila está vazia ao tentar retirar 
+(chamar) um cliente da fila..
+
+ */
+
+public class ExercicioFila{
+	static final Scanner sc= new Scanner(System.in);
+	
+	public static void main (String[] args) {
+		
+	
+
+        Queue<String> clientes = new LinkedList<>();
+        int option = -1;
+        String nomeCliente, removerCliente;
+
+        do {
+            System.out.println("""
+                    ___________________________________________________
+                                      Menu:
+                            1 - Adicionar cliente na Fila
+                            2 - Listar todos os Clientes
+                            3 - Retirar Cliente da Fila
+                            0 - Sair
+                    ___________________________________________________
+                    """);
+            System.out.println("Escolha uma opção do menu: ");
+            option = sc.nextInt();
+
+            switch (option) {
+                case 1:
+                    System.out.println("Adicionando novo cliente a fila: ");
+                    System.out.print("Digite o nome do cliente: ");
+                    sc.nextLine();
+                    nomeCliente = sc.nextLine();
+                    clientes.add(nomeCliente);
+                    System.out.println("Cliente adicionado a fila com sucesso!");
+                    break;
+
+                case 2:
+
+                    Iterator<String> iterator = clientes.iterator();
+
+                    while (iterator.hasNext()){
+                        System.out.println(iterator.next());
+                    }
+
+                    break;
+
+                case 3:
+                    if (clientes.size() == 0) {
+                        System.out.println("A fila esta vazia!");
+                    } else {
+                        System.out.println("Removendo cliente da fila: ");
+                        System.out.print("Escolha o cliente para ser retirado: ");
+                        sc.nextLine();
+                        removerCliente = sc.nextLine();
+                        if(clientes.contains(removerCliente)){
+                            clientes.remove(removerCliente);
+                            System.out.println("Cliente retirado da fila com sucesso!");
+                        } else {
+                            System.out.println("Cliente nao encontrado! Cancelando operação.");
+                        }
+                    }
+                    break;
+
+                case 0:
+                    System.out.println("Fechando o programa...");
+                    break;
+                default:
+                    System.out.println("Opção invalida! Tente novamente.");
+            }
+
+        } while (option != 0);
+
+        sc.close();
+    }
+}
+		
+			
